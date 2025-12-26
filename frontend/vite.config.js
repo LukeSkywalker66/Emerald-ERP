291f322d4125045a6927c6bdb790de0873c81cfd
Lucas <lucascaceres@gmail.com>
2025-12-26 09:04:48 -0300

    creando script para automatizar los certificados al levantar docker2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,        // Escuchar en 0.0.0.0 (Para Docker)
+    allowedHosts: true // 👈 LA CLAVE: Permitir cualquier dominio (emerald.2finternet.ar)
+  }
 })
